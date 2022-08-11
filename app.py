526496b78e6927b01660af316fa0af4df568f269
M-Rb3 <rabea9220@gmail.com>
2022-08-12 00:30:46 +0200

    update

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import json
 import imageio
 import base64
 import cv2
-import time
+import math
+
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
@@ -18,43 +19,35 @@ class Video(Resource):
 
     def post(self):
         url = json.loads(request.data)['url']
-        cap = cv2.VideoCapture(url)
-        print(url[:50])
+        duration = json.loads(request.data)['duration']
+        print(duration)
+        video = cv2.VideoCapture(url)
         image_lst = []
 
         while True:
             # Capture frame-by-frame
-            ret, frame = cap.read()
+            ret, frame = video.read()
+            # print(frame)
             # if frame is read correctly ret is True
             if not ret:
                 print("Can't receive frame (stream end?). Exiting ...")
                 break
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image_lst.append(frame_rgb)
+            image_lst.append(frame)
+        video.release()
 
-            key = cv2.waitKey(1)
-            if key == ord('q'):
-                break
-
-        cap.release()
-        cv2.destroyAllWindows()
-        print(image_lst)
-        # Convert to gif using the imageio.mimsave method
-        # imageio.mimsave('video1.gif', image_lst)
-        # start_time = time.time()
-        # myclip = ImageSequenceClip(image_lst, fps=30)
-        # myclip.to_gif('myClip.gif', logger=None)
-        # print("--- %s seconds ---" % (time.time() - start_time))
-        start_time = time.time()
+        frame_rgb = cv2.cvtColor(image_lst[0], cv2.COLOR_BGR2RGB)
+        update_imgs = [frame_rgb]
+        count = int(math.ceil(len(image_lst)/12))
+        for i in range(1, len(image_lst)-1, count):
+            frame_rgb = cv2.cvtColor(image_lst[i], cv2.COLOR_BGR2RGB)
+            update_imgs.append(frame_rgb)
+        frame_rgb = cv2.cvtColor(image_lst[-1], cv2.COLOR_BGR2RGB)
+        update_imgs.append(frame_rgb)
 
         gif_encoded = imageio.mimsave(
-            "<bytes>", image_lst, format='gif')
-        print("--- %s seconds ---" % (time.time() - start_time))
+            "<bytes>", update_imgs, format='gif', duration=0.5)
         encoded_string = gif_encoded.decode("ISO-8859-1")
 
-        # encoded_string = base64.b64encode(gif_encoded)
-        # encoded_string = b'data:image/gif;base64,'+encoded_string
-        # decoded_string = encoded_string.decode()
         return {"data": encoded_string}
 
 
